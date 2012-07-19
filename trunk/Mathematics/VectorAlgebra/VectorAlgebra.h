18f00727e49f9ecc7c5351773a9c82a73fed26d1
mech86@gmail.com <mech86@gmail.com@293049e9-5805-802f-77ca-10763fb99e56>
2012-07-19 06:08:49 +0000

    naming convention 통일 vector 함수는 prefix에 v, quaternion 함수는 prefix에 q, matrix 함수(추가예정)는 prefix에 m이 붙도록.

--- trunk/Mathematics/VectorAlgebra/VectorAlgebra.h
+++ trunk/Mathematics/VectorAlgebra/VectorAlgebra.h
@@ -1,55 +1,53 @@
 #ifndef VECTORALGEBRA_H
 #define VECTORALGEBRA_H
 
-float Dot3f(const float* a, const float* b);					// c = a . b
-void Cross3f(const float* a, const float* b, float* c);			// c = a x b
-float NormSquared3f(const float* x);							// norm(x)^2
-float Norm3f(const float* x);									// norm(x)
-void Normalize3f(float* x);										// x /= norm(x)
-void Assign3f(float* a, const float* b);						// a = b
-void Assign3f(float* a, const float b);							// a = (b, b, b)
-void AddVector3f(const float* a, const float* b, float* c);		// c = a + b
-void AddVector3f(float* a, const float* b);						// a += b
-void SubVector3f(const float* a, const float* b, float* c);		// c = a - b
-void SubVector3f(float* a, const float* b);						// a -= b
-void MultiplyScalar3f(float* x, const float& a);				// x *= a
-void DivideByScalar3f(float* x, const float& a);				// x /= a
+// 3D version
+float vDot3f(const float* a, const float* b);				// c = a . b
+void vCross3f(const float* a, const float* b, float* c);	// c = a x b
+float vNormSquared3f(const float* x);						// norm(x)^2
+float vNorm3f(const float* x);								// norm(x)
+void vNormalize3f(float* x);								// x /= norm(x)
+void vCopy3f(float* a, const float* b);						// a = b
+void vAssign3f(float* a, const float b);					// a = (b, b, b)
+void vAdd3f(const float* a, const float* b, float* c);		// c = a + b
+void vAdd3f(float* a, const float* b);						// a += b
+void vSub3f(const float* a, const float* b, float* c);		// c = a - b
+void vSub3f(float* a, const float* b);						// a -= b
+void vMulScalar3f(float* x, const float& a);				// x *= a
+void vDivScalar3f(float* x, const float& a);				// x /= a
 
-double Dot3d(const double* a, const double* b);					// c = a . b
-void Cross3d(const double* a, const double* b, double* c);		// c = a x b
-double NormSquared3d(const double* x);							// norm(x)^2
-double Norm3d(const double* x);									// norm(x)
-void Normalize3d(double* x);									// x /= norm(x)
-void Assign3d(double* a, const double* b);						// a = b
-void Assign3d(double* a, const double b);						// a = (b, b, b)
-void AddVector3d(const double* a, const double* b, double* c);	// c = a + b
-void AddVector3d(double* a, const double* b);					// a += b
-void SubVector3d(const double* a, const double* b, double* c);	// c = a - b
-void SubVector3d(double* a, const double* b);					// a -= b
-void MultiplyScalar3d(double* x, const double& a);				// x *= a
-void DivideByScalar3d(double* x, const double& a);				// x /= a
+double vDot3d(const double* a, const double* b);			// c = a . b
+void vCross3d(const double* a, const double* b, double* c);	// c = a x b
+double vNormSquared3d(const double* x);						// norm(x)^2
+double vNorm3d(const double* x);							// norm(x)
+void vNormalize3d(double* x);								// x /= norm(x)
+void vCopy3d(double* a, const double* b);					// a = b
+void vAssign3d(double* a, const double b);					// a = (b, b, b)
+void vAdd3d(const double* a, const double* b, double* c);	// c = a + b
+void vAdd3d(double* a, const double* b);					// a += b
+void vSub3d(const double* a, const double* b, double* c);	// c = a - b
+void vSub3d(double* a, const double* b);					// a -= b
+void vMulScalar3d(double* x, const double& a);				// x *= a
+void vDivScalar3d(double* x, const double& a);				// x /= a
 
 
 // 2D version
-float NormSquared2f(const float* x);							// norm(x)^2
-float Norm2f(const float* x);									// norm(x)
-void Normalize2f(float* x);										// x /= norm(x)
-float Dot2f(const float* a, const float* b);					// c = a . b
-void AddVector2f(const float* a, const float* b, float* c);		// c = a + b
-void AddVector2f(float* a, const float* b);						// a += b
-void SubVector2f(double* a, const double* b);					// a -= b
-void DivideByScalar2f(float* x, const float& a);				// x /= a
+float vNormSquared2f(const float* x);						// norm(x)^2
+float vNorm2f(const float* x);								// norm(x)
+void vNormalize2f(float* x);								// x /= norm(x)
+float vDot2f(const float* a, const float* b);				// c = a . b
+void vAdd2f(const float* a, const float* b, float* c);		// c = a + b
+void vAdd2f(float* a, const float* b);						// a += b
+void vSub2f(double* a, const double* b);					// a -= b
+void vDivScalar2f(float* x, const float& a);				// x /= a
 
-
-
-double NormSquared2d(const double* x);							// norm(x)^2
-double Norm2d(const double* x);									// norm(x)
-void Normalize2d(double* x);									// x /= norm(x)
-double Dot2d(const double* a, const double*b);					// c = a . b
-void AddVector2d(const double* a, const double* b, double* c);	// c = a + b
-void AddVector2d(double* a, const double* b);					// a += b
-void SubVector2d(double* a, const double* b);					// a -= b
-void DivideByScalar2d(double* x, const double& a);				// x /= a
-
+double vNormSquared2d(const double* x);						// norm(x)^2
+double vNorm2d(const double* x);							// norm(x)
+void vNormalize2d(double* x);								// x /= norm(x)
+double vDot2d(const double* a, const double*b);				// c = a . b
+void vAdd2d(const double* a, const double* b, double* c);	// c = a + b
+void vAdd2d(double* a, const double* b);					// a += b
+void vSub2d(double* a, const double* b);					// a -= b
+void vDivScalar2d(double* x, const double& a);				// x /= a
 
 #endif // VECTORALGEBRA_H
